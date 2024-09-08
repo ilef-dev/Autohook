4fa46ca9eae436b0ef37f057175d9854d1a2c6aa
ilef-dev <efanovis@yandex.ru>
2024-09-08 16:47:29 +0300

    develop

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,6 +31,8 @@
             button1 = new Button();
             progressBar1 = new ProgressBar();
             groupBox1 = new GroupBox();
+            richTextBox1 = new RichTextBox();
+            groupBox4 = new GroupBox();
             label1 = new Label();
             comboBox1 = new ComboBox();
             comboBox2 = new ComboBox();
@@ -38,15 +40,20 @@
             label2 = new Label();
             label3 = new Label();
             groupBox2 = new GroupBox();
+            groupBox3 = new GroupBox();
             linkLabel1 = new LinkLabel();
-            label4 = new Label();
+            label5 = new Label();
+            comboBox3 = new ComboBox();
             groupBox1.SuspendLayout();
+            groupBox4.SuspendLayout();
             groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(170, 435);
+            button1.Enabled = false;
+            button1.Location = new Point(167, 286);
             button1.Name = "button1";
             button1.Size = new Size(134, 52);
             button1.TabIndex = 0;
@@ -56,34 +63,60 @@
             // 
             // progressBar1
             // 
-            progressBar1.Location = new Point(6, 56);
+            progressBar1.Location = new Point(6, 131);
             progressBar1.Name = "progressBar1";
-            progressBar1.Size = new Size(385, 30);
+            progressBar1.Size = new Size(386, 30);
+            progressBar1.Style = ProgressBarStyle.Continuous;
             progressBar1.TabIndex = 1;
-            progressBar1.Click += progressBar1_Click;
             // 
             // groupBox1
             // 
-            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(richTextBox1);
+            groupBox1.Controls.Add(groupBox4);
             groupBox1.Controls.Add(progressBar1);
-            groupBox1.Location = new Point(41, 493);
+            groupBox1.Enabled = false;
+            groupBox1.Location = new Point(41, 344);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(397, 99);
+            groupBox1.Size = new Size(397, 231);
             groupBox1.TabIndex = 2;
             groupBox1.TabStop = false;
+            groupBox1.Text = "Активация";
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(6, 26);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.ReadOnly = true;
+            richTextBox1.Size = new Size(386, 99);
+            richTextBox1.TabIndex = 12;
+            richTextBox1.Text = "";
+            // 
+            // groupBox4
+            // 
+            groupBox4.Controls.Add(label1);
+            groupBox4.Location = new Point(100, 167);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(190, 58);
+            groupBox4.TabIndex = 11;
+            groupBox4.TabStop = false;
+            groupBox4.Text = "Состояние";
             // 
             // label1
             // 
-            label1.Location = new Point(6, 23);
+            label1.Enabled = false;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label1.Location = new Point(6, 21);
             label1.Name = "label1";
-            label1.Size = new Size(385, 30);
-            label1.TabIndex = 2;
+            label1.Size = new Size(178, 34);
+            label1.TabIndex = 0;
+            label1.Text = "Ожидание";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // comboBox1
             // 
+            comboBox1.Enabled = false;
             comboBox1.FormattingEnabled = true;
-            comboBox1.Items.AddRange(new object[] { "AccessRetail", "AccessVolume", "ExcelRetail", "ExcelVolume", "GrooveRetail", "GrooveVolume", "HomeBusinessPipcRetail", "HomeBusinessRetail", "HomeStudentRetail", "InfoPathRetail", "InfoPathVolume", "LyncAcademicRetail", "LyncEntryRetail", "LyncRetail", "LyncVolume", "MondoRetail", "MondoVolume", "OneNoteFreeRetail", "OneNoteRetail", "OneNoteVolume", "OutlookRetail", "OutlookVolume", "PersonalPipcRetail", "PersonalRetail", "PowerPointRetail", "PowerPointVolume", "ProPlusRetail", "ProPlusVolume", "ProfessionalPipcRetail", "ProfessionalRetail", "ProjectProRetail", "ProjectProVolume", "ProjectStdRetail", "ProjectStdVolume", "PublisherRetail", "PublisherVolume", "SPDRetail", "StandardRetail", "StandardVolume", "VisioProRetail", "VisioProVolume", "VisioStdRetail", "VisioStdVolume", "WordRetail", "WordVolume", "O365BusinessRetail", "O365HomePremRetail", "O365ProPlusRetail", "O365SmallBusPremRetail", "AccessRetail", "AccessRuntimeRetail", "AccessVolume", "ExcelRetail", "ExcelVolume", "HomeBusinessPipcRetail", "HomeBusinessRetail", "HomeStudentRetail", "HomeStudentVNextRetail", "MondoRetail", "MondoVolume", "OneNoteFreeRetail", "OneNoteRetail", "OneNoteVolume", "OutlookRetail", "OutlookVolume", "PersonalPipcRetail", "PersonalRetail", "PowerPointRetail", "PowerPointVolume", "ProPlusRetail", "ProPlusVolume", "ProfessionalPipcRetail", "ProfessionalRetail", "ProjectProRetail", "ProjectProVolume", "ProjectProXVolume", "ProjectStdRetail", "ProjectStdVolume", "ProjectStdXVolume", "PublisherRetail", "PublisherVolume", "SkypeServiceBypassRetail", "SkypeforBusinessEntryRetail", "SkypeforBusinessRetail", "SkypeforBusinessVolume", "StandardRetail", "StandardVolume", "VisioProRetail", "VisioProVolume", "VisioProXVolume", "VisioStdRetail", "VisioStdVolume", "VisioStdXVolume", "WordRetail", "WordVolume", "Access2019Retail", "Access2019Volume", "AccessRuntime2019Retail", "Excel2019Retail", "Excel2019Volume", "HomeBusiness2019Retail", "HomeStudent2019Retail", "Outlook2019Retail", "Outlook2019Volume", "Personal2019Retail", "PowerPoint2019Retail", "PowerPoint2019Volume", "ProPlus2019Retail", "ProPlus2019Volume", "Professional2019Retail", "ProjectPro2019Retail", "ProjectPro2019Volume", "ProjectStd2019Retail", "ProjectStd2019Volume", "Publisher2019Retail", "Publisher2019Volume", "SkypeforBusiness2019Retail", "SkypeforBusiness2019Volume", "SkypeforBusinessEntry2019Retail", "Standard2019Retail", "Standard2019Volume", "VisioPro2019Retail", "VisioPro2019Volume", "VisioStd2019Retail", "VisioStd2019Volume", "Word2019Retail", "Word2019Volume", "Access2021Retail", "Access2021Volume", "Excel2021Retail", "Excel2021Volume", "HomeBusiness2021Retail", "HomeStudent2021Retail", "OneNote2021Retail", "Outlook2021Retail", "Outlook2021Volume", "Personal2021Retail", "PowerPoint2021Retail", "PowerPoint2021Volume", "ProPlus2021Retail", "ProPlus2021Volume1", "ProPlusSPLA2021Volume", "Professional2021Retail", "ProjectPro2021Retail", "ProjectPro2021Volume1", "ProjectStd2021Retail", "ProjectStd2021Volume", "Publisher2021Retail", "Publisher2021Volume", "SkypeforBusiness2021Retail", "SkypeforBusiness2021Volume", "Standard2021Retail", "Standard2021Volume", "StandardSPLA2021Volume", "VisioPro2021Retail", "VisioPro2021Volume", "VisioStd2021Retail", "VisioStd2021Volume", "Word2021Retail", "Word2021Volume", "ProPlus2024Volume MAK", "ProjectPro2024Volume MAK", "VisioPro2024Volume MAK", "Access2024Retail", "Access2024Volume", "Excel2024Retail", "Excel2024Volume", "Home2024Retail", "HomeBusiness2024Retail", "Outlook2024Retail", "Outlook2024Volume", "PowerPoint2024Retail", "PowerPoint2024Volume", "ProjectPro2024Retail", "ProjectPro2024Volume1", "ProjectStd2024Retail", "ProjectStd2024Volume", "ProPlus2024Retail", "ProPlus2024Volume1", "SkypeforBusiness2024Volume", "Standard2024Volume1", "VisioPro2024Retail", "VisioPro2024Volume", "VisioStd2024Retail", "VisioStd2024Volume", "Word2024Retail", "Word2024Volume", "O365BusinessRetail", "O365EduCloudRetail", "O365HomePremRetail", "O365ProPlusRetail", "O365SmallBusPremRetail" });
-            comboBox1.Location = new Point(23, 57);
+            comboBox1.Location = new Point(100, 58);
             comboBox1.Name = "comboBox1";
             comboBox1.Size = new Size(240, 28);
             comboBox1.TabIndex = 4;
@@ -94,18 +127,18 @@
             comboBox2.Enabled = false;
             comboBox2.FormattingEnabled = true;
             comboBox2.Items.AddRange(new object[] { "Ru", "En" });
-            comboBox2.Location = new Point(296, 58);
+            comboBox2.Location = new Point(346, 58);
             comboBox2.Name = "comboBox2";
-            comboBox2.Size = new Size(79, 28);
+            comboBox2.Size = new Size(46, 28);
             comboBox2.TabIndex = 5;
             comboBox2.SelectedIndexChanged += comboBox2_SelectedIndexChanged;
             // 
             // button2
             // 
             button2.Enabled = false;
-            button2.Location = new Point(23, 100);
+            button2.Location = new Point(23, 105);
             button2.Name = "button2";
-            button2.Size = new Size(118, 29);
+            button2.Size = new Size(118, 30);
             button2.TabIndex = 6;
             button2.Text = "Генерировать";
             button2.UseVisualStyleBackColor = true;
@@ -114,17 +147,16 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(311, 23);
+            label2.Location = new Point(346, 35);
             label2.Name = "label2";
             label2.Size = new Size(43, 20);
             label2.TabIndex = 7;
             label2.Text = "Язык";
-            label2.Click += label2_Click;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(69, 34);
+            label3.Location = new Point(100, 35);
             label3.Name = "label3";
             label3.Size = new Size(101, 20);
             label3.TabIndex = 8;
@@ -132,7 +164,9 @@
             // 
             // groupBox2
             // 
-            groupBox2.Controls.Add(linkLabel1);
+            groupBox2.Controls.Add(groupBox3);
+            groupBox2.Controls.Add(label5);
+            groupBox2.Controls.Add(comboBox3);
             groupBox2.Controls.Add(label2);
             groupBox2.Controls.Add(label3);
             groupBox2.Controls.Add(comboBox1);
@@ -140,49 +174,70 @@
             groupBox2.Controls.Add(button2);
             groupBox2.Location = new Point(41, 59);
             groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(397, 146);
+            groupBox2.Size = new Size(397, 159);
             groupBox2.TabIndex = 9;
             groupBox2.TabStop = false;
             groupBox2.Text = "Выберите нужную версию";
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(linkLabel1);
+            groupBox3.Location = new Point(147, 92);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(242, 51);
+            groupBox3.TabIndex = 10;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Получить";
             // 
             // linkLabel1
             // 
             linkLabel1.AutoSize = true;
             linkLabel1.Enabled = false;
             linkLabel1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            linkLabel1.Location = new Point(197, 104);
+            linkLabel1.Location = new Point(6, 23);
             linkLabel1.Name = "linkLabel1";
-            linkLabel1.Size = new Size(168, 20);
+            linkLabel1.Size = new Size(221, 20);
             linkLabel1.TabIndex = 9;
             linkLabel1.TabStop = true;
-            linkLabel1.Text = "Оффициальная ссылка";
+            linkLabel1.Text = "Оффициальный сайт Microsoft";
+            linkLabel1.LinkClicked += linkLabel1_LinkClicked;
             // 
-            // label4
+            // label5
             // 
-            label4.AutoSize = true;
-            label4.Location = new Point(203, 208);
-            label4.Name = "label4";
-            label4.Size = new Size(50, 20);
-            label4.TabIndex = 10;
-            label4.Text = "label4";
-            label4.Click += label4_Click;
+            label5.AutoSize = true;
+            label5.Location = new Point(23, 35);
+            label5.Name = "label5";
+            label5.Size = new Size(33, 20);
+            label5.TabIndex = 12;
+            label5.Text = "Год";
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Items.AddRange(new object[] { "2013", "2016", "2021", "2024", "O365" });
+            comboBox3.Location = new Point(23, 58);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(71, 28);
+            comboBox3.TabIndex = 11;
+            comboBox3.SelectedIndexChanged += comboBox3_SelectedIndexChanged;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(482, 653);
-            Controls.Add(label4);
             Controls.Add(groupBox2);
             Controls.Add(groupBox1);
             Controls.Add(button1);
             Name = "Form1";
-            Text = "MicrOp ver1.0";
+            Text = "Brod ver1.0";
             groupBox1.ResumeLayout(false);
+            groupBox4.ResumeLayout(false);
             groupBox2.ResumeLayout(false);
             groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -190,7 +245,6 @@
         private Button button1;
         private ProgressBar progressBar1;
         private GroupBox groupBox1;
-        private Label label1;
         private ComboBox comboBox1;
         private ComboBox comboBox2;
         private Button button2;
@@ -198,6 +252,11 @@
         private Label label3;
         private GroupBox groupBox2;
         private LinkLabel linkLabel1;
-        private Label label4;
+        private Label label5;
+        private ComboBox comboBox3;
+        private GroupBox groupBox3;
+        private GroupBox groupBox4;
+        private Label label1;
+        private RichTextBox richTextBox1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,7 @@
+using brod;
+using System.Diagnostics;
+using System.Windows.Forms;
+
 namespace microp
 {
     public partial class Form1 : Form
@@ -5,53 +9,164 @@ namespace microp
         public Form1()
         {
             InitializeComponent();
+            LoadOfficesAsync();
+        }
+
+        private async void LoadOfficesAsync()
+        {
+            // Запускаем загрузку данных в отдельном потоке
+            await Task.Run(() =>
+            {
+                OfficeLoader.Load();
+            });
+        }
+
+        private async Task<List<String>> LoadOfficesByYearAsync(string year)
+        {
+            List<String> data = new List<String>();
+            // Запускаем загрузку данных в отдельном потоке
+            await Task.Run(() =>
+            {
+                List<OfficeLoader.Office> offices = OfficeLoader.GetOfficesByYear(year);
+                foreach (OfficeLoader.Office office in offices)
+                {
+                    data.Add(office.name);
+                }
+            });
+
+            return data;
+        }
+
+        private void UpdateComboBox(List<string> data)
+        {
+            // Проверка, что обновление происходит в основном потоке
+            if (InvokeRequired)
+            {
+                // Используем Invoke для обновления элементов управления из другого потока
+                Invoke(new Action(() => UpdateComboBox(data)));
+                return;
+            }
+
+            comboBox1.Items.Clear();
+            foreach (var item in data)
+            {
+                comboBox1.Items.Add(item);
+            }
         }
 
         private async void button1_Click(object sender, EventArgs e)
         {
+            int result;
             button1.Enabled = false;
-            label1.Text = "Создание символической ссылки.";
-            int result = await Task.Run(() => OfficeActivation.CreateMklink());
+            label1.Enabled = true;
 
+            richTextBox1.AppendText("Создание символической ссылки для sppc.dll" + Environment.NewLine);
+            result = await Task.Run(() => OfficeActivation.CreateMklink());
+            if (result == 0)
+            {
+                richTextBox1.AppendText("Не удалось создать символическую ссылку для sppc.dll" + Environment.NewLine);
+                label1.Text = "Ошибка";
+                label1.ForeColor = Color.Red;
+                progressBar1.Enabled = false;
+                return;
+            }
             progressBar1.Value = 10;
 
+            richTextBox1.AppendText("Добавление модифицированной библиотки sppcs.dll" + Environment.NewLine);
             result = await Task.Run(() => OfficeActivation.UploadSppc64());
+            if (result == 0)
+            {
+                richTextBox1.AppendText("Не удалось добавить модифицированную библиотку sppcs.dll" + Environment.NewLine);
+                label1.Text = "Ошибка";
+                label1.ForeColor = Color.Red;
+                progressBar1.Enabled = false;
+                return;
+            }
             progressBar1.Value = 90;
 
+            richTextBox1.AppendText("Активация ключа" + Environment.NewLine);
+            richTextBox1.AppendText("Нажмите OK" + Environment.NewLine);
             result = await Task.Run(() => OfficeActivation.InsertKey());
+            if (result == 0)
+            {
+                richTextBox1.AppendText("Не удалось активировать ключ" + Environment.NewLine);
+                label1.Text = "Ошибка";
+                label1.ForeColor = Color.Red;
+                progressBar1.Enabled = false;
+                return;
+            }
             progressBar1.Value = 100;
+
+            label1.Text = "Успешно";
             label1.ForeColor = Color.Green;
-            label1.Text = "Complete";
-        }
-
-        private void progressBar1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label4_Click(object sender, EventArgs e)
-        {
-
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            linkLabel1.Enabled = false;
             comboBox2.Enabled = true;
+            button1.Enabled = true;
+            groupBox1.Enabled = true;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            string link = @"https://officecdn.microsoft.com/db/492350f6-3a01-4f97-b9c0-c7c6ddf67d60/media";
+            if (comboBox2.SelectedItem == "Ru")
+            {
+                link += "/ru-ru";
+            }
+            else
+            {
+                link += "/en-us";
+            }
+            link += "/" + comboBox1.SelectedItem + ".img";
+
+            linkLabel1.Links.Clear();
+            linkLabel1.Links.Add(0, linkLabel1.Text.Length, link);
+
             linkLabel1.Enabled = true;
+
         }
 
         private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
         {
+            linkLabel1.Enabled = false;
             button2.Enabled = true;
+        }
+
+        private async void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            List<string> offices = await LoadOfficesByYearAsync(comboBox3.SelectedItem as String);
+
+            comboBox1.SelectedIndex = -1;
+            comboBox2.SelectedIndex = -1;
+            button2.Enabled = false;
+            linkLabel1.Enabled = false;
+            comboBox1.Items.Clear();
+
+            foreach (string office in offices)
+            {
+                comboBox1.Items.Add(office);
+            }
+
+            comboBox1.Enabled = true;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Открытие ссылки в браузере
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = e.Link.LinkData.ToString(), // URL для открытия
+                UseShellExecute = true // Использовать ассоциации файлов для открытия URL
+            };
+            System.Diagnostics.Process.Start(startInfo);
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
